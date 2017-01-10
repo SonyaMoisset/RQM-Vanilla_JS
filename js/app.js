9299e657c877b82a80cf20dbef25b32096b18be2
Sonya Moisset <sonya.moisset@gmail.com>
2017-01-10 00:48:43 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,61 @@
 let dataList = [
-  "I should just tell her what she wants to hear.",
-  "I also have security flaws. I don't like being outside. I liked morphine too much.",
-  "I only need to press one key to run the exploit. Or, I can press another and disable the entire plan.",
-  "I don't know what I'm supposed to do.",
-  "We live in a kingdom of bullshit.",
-  "I'm only supposed to be your prophet. You're supposed to be my God.",
-  "Hackers. We inherently trust no one, including each other.",
-  "There, that's the look I was looking for.",
-  "I'm just playing my best move.",
-  "Trust yourself. You'll do what's right.",
-  "Unfortunately, we're all human. Except me. of course.",
-  "I made my move. You make yours",
-  "Life is so much easier when you're numb"
+  {
+    quote: "I should just tell her what she wants to hear.",
+    image: "id-1.jpg"
+  },
+  {
+    quote: "I also have security flaws. I don’t like being outside. I liked morphine too much.",
+    image: "id-2.jpg"
+  },
+  {
+    quote: "I only need to press one key to run the exploit. Or, I can press another and disable the entire plan.",
+    image: "id-3.jpg"
+  },
+  {
+    quote: "I don't know what I'm supposed to do.",
+    image: "id-4.jpg"
+  },
+  {
+    quote: "We live in a kingdom of bullshit.",
+    image: "id-5.jpg"
+  },
+  {
+    quote: "I'm only supposed to be your prophet. You're supposed to be my God.",
+    image: "id-6.jpg"
+  },
+  {
+    quote: "I'm just playing my best move.",
+    image: "id-7.jpg"
+  },
+  {
+    quote: "Trust yourself. You'll do what's right.",
+    image: "id-8.jpg"
+  },
+  {
+    quote: "Unfortunately, we're all human. Except me. of course.",
+    image: "id-9.jpg"
+  },
+  {
+    quote: "I made my move. You make yours",
+    image: "id-10.jpg"
+  }
 ];
 
-function randomQuote() {
-  let index = Math.floor(Math.random() * (dataList.length - 1));
-  return dataList[index];
+let quoteEl = document.getElementById("quote-body");
+let imageEl = document.getElementById("quote-image");
+let randomQuoteBtnEl = document.getElementById("quote-btn");
+
+function randomData() {
+  let newData = dataList[index(0, dataList.length - 1)];
+
+  quoteEl.innerHTML = newData.quote;
+  imageEl.setAttribute('src', 'img/' + newData.image);
+
+  function index(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
-function showQuote() {
-  var quoteEl = document.getElementById("quote-body");
-  quoteEl.innerHTML = randomQuote();
-}
-
-document
-  .getElementById("quote-btn")
-  .addEventListener("click", function(e) {
-    e.preventDefault();
-    showQuote();
-  })
+randomQuoteBtnEl.addEventListener("click", function(e) {
+    randomData(dataList, quoteEl, imageEl);
+});
